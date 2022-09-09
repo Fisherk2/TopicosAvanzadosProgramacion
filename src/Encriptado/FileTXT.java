375b686a307e04f03668296240659f5f06d43283
Fisherk2 <fisher_k2@hotmail.com>
2022-09-09 08:13:16 -0500

    Version 1.0 9-09-22

--- src/Encriptado/FileTXT.java
+++ src/Encriptado/FileTXT.java
@@ -21,7 +21,6 @@ import javax.swing.JOptionPane;
 public class FileTXT {
 
     private File txt;
-    private String path;
 
     public FileTXT() {
         creacionFichero();
@@ -29,14 +28,16 @@ public class FileTXT {
 
     private void creacionFichero() {
         try {
-            txt = new File("C:\\Users\\msi\\Desktop\\ITL\\5to semestre\\Topicos Avanzados de programacion\\Topicos Programacion\\Entrada.txt");
-            if (txt.createNewFile()) {//Si el archivo a sido creado, enviara un mensaje
+            txt = new File(
+                    "C:\\Users\\msi\\Desktop\\ITL\\5to semestre\\Topicos Avanzados de programacion\\Topicos Programacion\\Entrada.txt");
+            if (txt.createNewFile()) {// Si el archivo a sido creado, enviara un mensaje
                 System.out.println("\n---------------------------\n"
                         + "El archivo TXT se ha creado exitosamente");
             }
-        } catch (IOException ex) { //serr + tabulador = System.err.println();
+        } catch (IOException ex) { // serr + tabulador = System.err.println();
             System.err.println("NO SE PUDO CREAR EL ARCHIVO " + ex);
-            JOptionPane.showMessageDialog(null, "ERROR, EL ARCHIVO NO PUEDE CREARSE, VERIFIQUE BIEN LA DIRECCION ABSOLUTA DEL FICHERO");
+            JOptionPane.showMessageDialog(null,
+                    "ERROR, EL ARCHIVO NO PUEDE CREARSE, VERIFIQUE BIEN LA DIRECCION ABSOLUTA DEL FICHERO");
         }
     }
 
@@ -44,12 +45,14 @@ public class FileTXT {
         try {
             FileWriter escribir = new FileWriter(txt);
             escribir.write(entrada);
-            //Siempre que se va utilizar la clase FileWriter se debe cerrar el archivo cuando se termine de utilizar.
+            // Siempre que se va utilizar la clase FileWriter se debe cerrar el archivo
+            // cuando se termine de utilizar.
             escribir.close();
 
         } catch (IOException ex) {
             System.err.println("ERROR, EL ARCHIVO NO SE PUDO ESCRIBIR SOBRE EL ARCHIVO " + ex);
-            JOptionPane.showMessageDialog(null, "ERROR, EL ARCHIVO NO PUEDE ESCRIBIRSE, VERIFIQUE BIEN LA DIRECCION ABSOLUTA DEL FICHERO");
+            JOptionPane.showMessageDialog(null,
+                    "ERROR, EL ARCHIVO NO PUEDE ESCRIBIRSE, VERIFIQUE BIEN LA DIRECCION ABSOLUTA DEL FICHERO");
         }
 
     }
@@ -61,20 +64,24 @@ public class FileTXT {
             FileReader lector = new FileReader(txt);
             BufferedReader lectura = new BufferedReader(lector);
 
-            cadena = lectura.readLine(); //Leera la primer linea que se encuentra en el fichero hasta que llegue a un null
+            cadena = lectura.readLine(); // Leera la primer linea que se encuentra en el fichero hasta que llegue a un
+                                         // null
 
-            while (cadena != null) {//Si no hay vacios en nuestro archivos, entonces se tiene que seguir leyendo el archivo
+            while (cadena != null) {// Si no hay vacios en nuestro archivos, entonces se tiene que seguir leyendo el
+                                    // archivo
                 System.out.println("\n" + cadena);
                 contenido += cadena + "\n";
-                cadena = lectura.readLine(); //Volvera a leer la siguiente linea hasta encontrar con un vacio
+                cadena = lectura.readLine(); // Volvera a leer la siguiente linea hasta encontrar con un vacio
             }
 
         } catch (FileNotFoundException ex) {
             System.err.println("ERROR, NO SE PUEDE ENCONTRAR EL ARCHIVO " + ex);
-            JOptionPane.showMessageDialog(null, "ERROR, EL ARCHIVO NO PUEDE ENCONTRARSE, VERIFIQUE BIEN LA DIRECCION ABSOLUTA DEL FICHERO");
+            JOptionPane.showMessageDialog(null,
+                    "ERROR, EL ARCHIVO NO PUEDE ENCONTRARSE, VERIFIQUE BIEN LA DIRECCION ABSOLUTA DEL FICHERO");
         } catch (IOException ex) {
             System.err.println("ERROR, NO SE PUEDE LEER EL ARCHIVO " + ex);
-            JOptionPane.showMessageDialog(null, "ERROR, EL ARCHIVO NO PUEDE LEER, VERIFIQUE BIEN EL CONTENIDO DEL FICHERO");
+            JOptionPane.showMessageDialog(null,
+                    "ERROR, EL ARCHIVO NO PUEDE LEER, VERIFIQUE BIEN EL CONTENIDO DEL FICHERO");
 
         }
 
